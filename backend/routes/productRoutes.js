4eb8e375c28f558825b482347c70307e4418d4d7
OdilovMM <dssrinman@gmail.com>
2024-01-08 13:23:17 +0900

    add: asyncHandler and error handler middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const products = require("../data/products");
+const asyncHandler = require("../middleware/asyncHandler");
+const Product = require("../models/productModel");
 
-router.get("/", (req, res) => {
-  res.status(200).json(products);
-  // console.log(products)
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
 
-router.get("/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-  // console.log(products)
-});
+    res.status(200).json(products);
+  })
+);
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.status(200).json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  })
+);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,28 +6,13 @@ const cors = require("cors");
 const connectDB = require("./config/db.js");
 const productRoutes = require("./routes/productRoutes.js");
 connectDB();
+const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 const port = process.env.PORT || 5000;
 
-
-
-
 app.use(cors());
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 app.use("/api/products", productRoutes);
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
